559d3a54bd930a4546a03e43280a5dee18c304e2
신준규 <tlswnsrb1357@gmail.com>
2026-07-24 11:13:18 +0900

    스핀락 구현 시작

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,37 @@
 ﻿#include "pch.h"
 #include "CorePch.h"
 
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+int32 sum = 0;
+std::mutex m;
+
+void Add()
+{
+	for (int32 i = 0; i < 10'0000; ++i)
+	{
+		std::lock_guard<std::mutex>guard(m);
+		sum++;
+	}
+}
+void Sub()
+{
+	for (int32 i = 0; i < 10'0000; ++i)
+	{
+		std::lock_guard<std::mutex>guard(m);
+		sum--;
+	}
+}
+
 int main()
 {
-    HelloWorld();
+	std::thread t1(Add);
+	std::thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+	std::cout << sum << "\n";
 }
