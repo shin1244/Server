c43f76c00cc7546e7ec54a513c9689190186b2ef
신준규 <tlswnsrb1357@gmail.com>
2026-07-27 16:02:41 +0900

    ConcurrentQueue 제작 완료

--- GameServer/ConcurrentQueue.h
+++ GameServer/ConcurrentQueue.h
@@ -1,5 +1,51 @@
 #pragma once
-class ConcurrentQueue
+#include <mutex>
+#include <queue>
+#include <stack>
+
+template<typename T>
+class LockQueue
 {
+public:
+	LockQueue() {}
+	LockQueue(const LockQueue&) = delete;
+	LockQueue& operator = (const LockQueue&) = delete;
+
+	void Push(T val)
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		queue_.push(std::move(val));
+		condVar_.notify_one();
+	}
+
+	bool Pop(T& val)
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (queue_.empty())
+			return false;
+
+		val = std::move(queue_.front());
+		queue_.pop();
+		return true;
+	}
+
+	bool Empty()
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		return queue_.empty();
+	}
+
+	void WaitPop(T& val)
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+		condVar_.wait(lock, [this] {return !queue_.empty(); });
+		val = std::move(queue_.front());
+		queue_.pop();
+	}
+
+private:
+	std::queue<T> queue_;
+	std::mutex mutex_;
+	std::condition_variable condVar_;
 };
 
