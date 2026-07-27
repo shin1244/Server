9faf872bdab1b8d642f060a325dd2c5aa9a4b87e
신준규 <tlswnsrb1357@gmail.com>
2026-07-27 14:34:11 +0900

    원자적 연산 연습 중

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,42 +8,11 @@
 #include <windows.h>
 #include <chrono>
 
-std::mutex m;
-std::queue<int32> q;
-HANDLE handle;
-
-void Producer()
-{
-	while (true)
-	{
-		std::unique_lock<std::mutex> lock(m);
-		q.push(100);
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		SetEvent(handle);
-	}
-}
-
-void Consumer()
-{
-	while (true)
-	{
-		WaitForSingleObject(handle, -1);
-		std::unique_lock<std::mutex> lock(m);
-		if (!q.empty())
-		{
-			int32 data = q.front();
-			q.pop();
-			std::cout << data << "\n";
-		}
-	}
-}
-
 int main()
 {
-	handle = CreateEvent(NULL, FALSE, FALSE, NULL);
-	std::thread t1(Producer);
-	std::thread t2(Consumer);
+	std::atomic<bool> flag = false;
 
-	t1.join();
-	t2.join();
+	flag.store(true);
+
+	bool val = flag.load();
 }
